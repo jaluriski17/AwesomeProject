165804504973d8b23a8faa1c47bf9de26403b885
jaluriski17 <jaluriskiprasetyo20004@mail.ugm.ac.id>
2024-11-11 08:12:13 +0700

    API

--- BottomNav.js
+++ BottomNav.js
@@ -2,16 +2,27 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesomeIcon } from'@fortawesome/react-native-fontawesome';
+import { faUser,} from '@fortawesome/free-regular-svg-icons';
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons';
+import Mahasiswa from './Mahasiswa';
+import { WebView } from 'react-native-webview';
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
+    
+      <Mahasiswa/>
+  );
+}
+function WebScreen() {
+  return (
+    
+      <WebScreen
+      sourec = {{uri:'https://github.com/jaluriski17'}}/>
   );
 }
 
-function SettingsScreen() {
+function DatamahasiswaScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Settings!</Text>
@@ -25,8 +36,34 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Profil" component={HomeScreen} 
+        options={{ 
+          headerShown: false,
+          tabBarIcon: ({color})=> (
+            <FontAwesomeIcon icon ={faUser} color ={color} size={32}/>
+          ),
+          }} /> 
+
+       <Tab.Screen name="GitHub" component={WebScreen} 
+        options={{ 
+          headerShown: false,
+
+          tabBarIcon: ({color})=> (
+            <FontAwesomeIcon icon ={faUser} color ={color} size={32}/>
+          ),
+          }} /> 
+
+        <Tab.Screen name="Data Mahasiswa " component={DatamahasiswaScreen} 
+        
+        options={{ 
+          headerShown: false,
+          tabBarIcon: ({color})=> (
+            <FontAwesomeIcon icon ={faGraduationCap} color ={color} size={32}/>
+          ),
+          }}
+
+
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
